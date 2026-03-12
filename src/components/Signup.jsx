e64da984762fd7523ee9105ab7fd549771e67cc3
Brayemar <mogikoyob@gmail.com>
2026-03-12 14:06:28 +0300

    updates

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
@@ -8,14 +9,62 @@ const Signup = () => {
   const [password, setPassword] = useState("");
   const [phone, setPhone] = useState("");
 
+// Define the three states an application will move to 
+const [loading, setLoading] = useState("");
+const [success, setSuccess] = useState("");
+const [error, setError] = useState("");
 
+// Below is a function that will handle the submit action 
+const handleSubmit = async(e) =>{
+  // Below we prevent our site from reloading 
+  e.preventDefault()
+
+  // Update our loading hook with a message that will be displayed to the users who are trying to register 
+  setLoading("Please wait as registration is in progress...")
+  try{
+    // create a form data object that will enable you to capture the four details entered on the form 
+    const formdata = new FormData();
+
+    // insert the for details (username, email, password, phone)in terms of key - value pairs 
+    formdata.append("username", username);
+    formdata.append("email", email);
+    formdata.append("password", password);
+    formdata.append("phone", phone);
+
+    // by use of axios, we can access the method POST
+    const response =await axios.post("https://brayemar.alwaysdata.net/api/signup", formdata)
+
+    // set back loading hook back to default
+    setLoading("");
+
+    // just incase everything goes on well, update the success hook with a message
+    setSuccess(response.data.message)
+
+    // clear your hooks
+    setUsername("")
+    setEmail("")
+    setPassword("")
+    setPhone("")
+  }
+  catch(error){
+    // set the loading back to default
+    setLoading("");
+
+    // update the error hook with the message given back from the response
+    setError(error.message)
+  }
+}
 
   return (
     <div className='row justify-content-center mt-4'>
       <div className="card col-md-6 shadow p-4">
         <h1 className='text-primary'>Sign Up</h1>
 
-        <form>
+        <h1 className='text-warning'> {loading} </h1>
+        <h3 className="text-success"> {success} </h3>
+        <h4 className="text-danger"> {error} </h4>
+
+        <form onSubmit={handleSubmit}>
           <input type="text"
           placeholder='Enter the Username'
           className='form-control'
@@ -52,7 +101,7 @@ const Signup = () => {
 
           {phone}
 
-          <input type="button"
+          <input type="submit"
           value="Signup"
           className='btn btn-primary' /> <br /> <br />
 
